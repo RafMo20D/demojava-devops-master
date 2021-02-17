c8cc4a2b368b9ba5c12c5185cb392c336810f007
RafMo20D <67839308+RafMo20D@users.noreply.github.com>
2021-02-17 07:56:20 -0500

    Update Person.java

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -2,8 +2,8 @@ package com.example.model;
 
 public class Person {
 	//Test comment added
-	// add comment 2
-	// test adding email notification
+	//add comment 2
+	//test adding email notification
 	String id;
 	String firstName;
 	String lastName;
