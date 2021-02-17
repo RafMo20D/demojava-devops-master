ffd4062c670dd66280a2e15f6b42290cfb9c317e
RafMo20D <67839308+RafMo20D@users.noreply.github.com>
2021-02-17 07:00:27 -0500

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -33,12 +33,7 @@ public class PersonService {
 		p.setLastName("Salama");
 		persons.add(p);
 		
-		p = new Person();
-		p.setId("4");
-		p.setAge(25);
-		p.setFirstName("AbdulHalim");
-		p.setLastName("Mahmoud");
-		persons.add(p);
+		
 		
 	}
 	public Person getPerson(String id) {
